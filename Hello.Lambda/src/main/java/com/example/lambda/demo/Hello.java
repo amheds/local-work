d495fe4b3d50cd418145089893206cda675a08ae
amheds <amheds@gmail.com>
2020-09-15 11:51:09 -0400

    Initial commit

---
+++ Hello.Lambda/src/main/java/com/example/lambda/demo/Hello.java
@@ -0,0 +1,18 @@
+package com.example.lambda.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Hello implements RequestHandler<Object, String> {
+
+    @Override
+    public String handleRequest(Object input, Context context) {
+        context.getLogger().log("Input: " + input);
+
+        // TODO: implement your handler
+       String output = "Hello, " + input + "!"; 
+    		   
+        return output;
+    }
+
+}
